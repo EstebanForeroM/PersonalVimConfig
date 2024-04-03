c42000436b4ab4c90019e696491927111713088f
Esteban Forero <estebanffm@outlook.com>
2024-04-03 18:10:23 -0500

    updated changes

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,6 @@ return {
     config = function()
 	vim.cmd.colorscheme "catppuccin-mocha"
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
     end
 }
